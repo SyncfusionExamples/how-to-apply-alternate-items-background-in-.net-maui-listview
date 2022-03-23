5a7afcd2d21289c66f4b58c65ae125fea1df6584
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2022-03-23 14:50:41 +0530

    Add files via upload

---
+++ ListViewMaui/Helper/Behavior.cs
@@ -0,0 +1,37 @@
+﻿using Syncfusion.Maui.ListView;
+
+namespace ListViewMaui
+{
+    public class Behavior : Behavior<ContentPage>
+    {
+        SfListView ListView;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            ListView = bindable.FindByName<SfListView>("listView");
+            ListView.SelectionChanging += ListView_SelectionChanging;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            ListView.SelectionChanging -= ListView_SelectionChanging;
+            ListView = null;
+            base.OnDetachingFrom(bindable);
+        }
+
+        private void ListView_SelectionChanging(object sender, ItemSelectionChangingEventArgs e)
+        {
+            for (int i = 0; i < e.AddedItems.Count; i++)
+            {
+                var item = e.AddedItems[i] as Contacts;
+                item.IsSelected = true;
+            }
+
+            for (int i = 0; i < e.RemovedItems.Count; i++)
+            {
+                var item = e.RemovedItems[i] as Contacts;
+                item.IsSelected = false;
+            }
+        }
+    }
+}
---
+++ ListViewMaui/Helper/IndexToColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using Syncfusion.Maui.ListView;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewMaui
+{
+    public class IndexToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var listview = parameter as SfListView;
+            return listview.DataSource.DisplayItems.IndexOf(value) % 2 == 0 ? Colors.Lavender : Colors.AliceBlue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
